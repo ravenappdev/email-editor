4dc7e6c02131f355b4eb5064bd596207bc92e950
priyansh srivastava <priyansh@ravenapp.dev>
2022-01-01 14:49:26 +0530

    changing server api url

--- app/src/core/api/exportHtmlService.js
+++ app/src/core/api/exportHtmlService.js
@@ -3,24 +3,23 @@ class ExportHtmlService {
     generateHtml = jsx =>
         new Promise((resolve, reject) => {
             //check if url is present in env.
-            let url = window.location.origin + "/api/html";
+            let url = null;
             if (process.env.REACT_APP_SSR_EXPORT_HTML_URL) {
                 url = process.env.REACT_APP_SSR_EXPORT_HTML_URL;
+                axios
+                    .post(url, { app: jsx }, { headers: { "Content-Type": "application/json" } })
+                    .then(response => {
+                        if (response) {
+                            resolve(response.data);
+                        } else {
+                            reject(response);
+                        }
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        reject(error);
+                    });
             }
-            console.log(url);
-            axios
-                .post(url, { app: jsx }, { headers: { "Content-Type": "application/json" } })
-                .then(response => {
-                    if (response) {
-                        resolve(response.data);
-                    } else {
-                        reject(response);
-                    }
-                })
-                .catch(error => {
-                    console.log(error);
-                    reject(error);
-                });
         });
 }
 
